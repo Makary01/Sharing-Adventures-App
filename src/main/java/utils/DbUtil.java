54f9083acf18ada3108b9278534a9b495a9fa96d
Makary01 <makary.bortnowski@gmail.com>
2020-11-27 15:20:23 +0100

    web and database connection

---
+++ src/main/java/utils/DbUtil.java
@@ -0,0 +1,31 @@
+package utils;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DbUtil {
+    private static DataSource dataSource;
+
+    //Here you can write Your database name
+    private static final String DATABASE_NAME = "";
+
+    public static Connection getConnection() throws SQLException {
+        return getInstance().getConnection();
+    }
+
+    private static DataSource getInstance() {
+        if (dataSource == null) {
+            try {
+                Context context = new InitialContext();
+                dataSource = (DataSource) context.lookup("java:comp/env/" + DATABASE_NAME);
+            } catch (NamingException e) {
+                e.printStackTrace();
+            }
+        }
+        return dataSource;
+    }
+}
